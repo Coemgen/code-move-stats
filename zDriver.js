a6e45f66b5fb264d97259e017f82bdbb384e807a
Coemgen <coemgen@hotmail.com>
2020-07-12 15:40:39 -0400

    adding emailer for yearly stats sheet

--- zDriver.js
+++ zDriver.js
@@ -40,3 +40,66 @@ function zInitStatsTemplateMain() {
   "use strict";
   zInitStatsTemplate.main();
 }
+
+
+/**
+ * Function to be called by a weekly {@linkcode
+ * https://developers.google.com/apps-script/guides/triggers/installable
+ * Trigger} to send a reminder for the Code Move Group to update the
+ * spreadsheet.
+ * @function sendWeeklyReminder
+ * @memberof Drivers
+ * @public
+ */
+// eslint-disable-next-line no-unused-vars
+function zsendMonthlyOhsStatsReminder() {
+  "use strict";
+  
+  // Pseudo Code
+  // 1) Confirm it's the first of the month, before sending out an email
+  // 2) Get file URL for email message body
+  // 3) Compose email contents/options
+  // 4) Send email
+  
+  // Comment line below for testing
+  //if ( (new Date().getMonth() !== 1) ) return; // Not the 1st.
+  
+  var strYear = new Date().getFullYear().toString().trim();
+  var folderData = DriveApp.getFolderById( PropertiesService.getScriptProperties().getProperty("dataFolderId") );
+  var folderIterator = folderData.getFoldersByName( strYear );
+  var folderYear = (folderIterator.hasNext()) ? folderIterator.next() : false;
+  
+  if ( !folderYear )
+  {
+    Logger.log("YYYY Folder was not found: " + strYear);
+    return; // No matching YYYY folder found
+  }
+  
+  var fileName = "Weekend Days OHS Stat tracking information " + strYear;
+  var fileIterator = folderYear.getFilesByName( fileName );
+  var fileOhsStats = (fileIterator.hasNext()) ? fileIterator.next() : false;
+  
+  if ( !fileOhsStats ) {
+    Logger.log("OHS Stats spreadsheet was not found: " + fileName);
+    return; // No matching OHS Stat sheet found
+  }
+  
+  var strFileUrl = fileOhsStats.getUrl();
+  
+  var recipients = "jeburns@meditech.com";
+  var subject = "MONTHLY: " + fileName;
+  
+  var body  = '<p>Click the following link to access the sheet: <a href="{file.getUrl}">{file.getName}</a></p>';
+      body += '<p>If you see !#REF in the cell, click the cell, and then click Allow Access to connect the data.</p>';
+      body += '<p>Please enter all non-automated data values.</p>';
+      body += '<p>If you have any questions/comments, please contact James E Burns or Kevin Griffin.</p>';
+  
+      body = body.replace(/\{file.getName\}/g, fileOhsStats.getName())
+                 .replace(/\{file.getUrl\}/g, fileOhsStats.getUrl());
+  
+  
+  var options = {htmlBody: body};
+
+  // ---- Use Gamil Service to send email(s) ----
+  GmailApp.sendEmail(recipients, subject, body, options);
+}
