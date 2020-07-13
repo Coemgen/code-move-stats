a4e2e973d474025847cee7c0518f1e2d37898178
Coemgen <coemgen@hotmail.com>
2020-07-13 07:30:26 -0400

    wip

--- MonthlyRun.js
+++ MonthlyRun.js
@@ -1,6 +1,7 @@
 /*jslint browser:true, long:true, white:true*/
 /*global
-DriveApp, FIRST_STAFF_ROW, PropertiesService, SendEmail, SpreadsheetApp
+DriveApp, FIRST_STAFF_ROW, PropertiesService, SendEmail, SitesApp,
+SpreadsheetApp
 */
 
 /**
@@ -17,7 +18,9 @@ DriveApp, FIRST_STAFF_ROW, PropertiesService, SendEmail, SpreadsheetApp
  *  <li><b>dataFolderId</b>&nbsp;&ndash;&nbsp;the parent folder for yearly data
  *  folders</li>
  *  <li><b>codeMoveTemplateId</b>&nbsp;&ndash;&nbsp;the spreadsheet id for the
- *  montly totals template</li>
+ *  monthly totals template</li>
+ *  <li><b>googleSiteUrl</b>&nbsp;&ndash;&nbsp;the url for the project's
+ *  associated Google Site</li>
  * </ul>
  * @author Kevin Griffin <kevin.griffin@gmail.com>
  */
@@ -31,8 +34,38 @@ DriveApp, FIRST_STAFF_ROW, PropertiesService, SendEmail, SpreadsheetApp
 // eslint-disable-next-line no-unused-vars
 const MonthlyRun = (
 
-  function (DriveApp, PropertiesService, SpreadsheetApp) {
+  function (DriveApp, PropertiesService, SitesApp, SpreadsheetApp) {
     "use strict";
+
+    /**
+     * Creates and returns a new yearly stats file object.  Also adds the stats
+     * file's url to the associated Google Site.
+     * @function addYearlyStatsFile
+     * @memberof MonthlyRun
+     * @private
+     * @param {Object} yearlyStatsTemplate
+     * @param {string} yearStr
+     * @param {Object} yearFolder
+     * @returns {Object}
+     */
+    function addYearlyStatsFile(yearlyStatsTemplate, yearStr, yearFolder) {
+      const yearlyStatsFile = DriveApp.getFileById(yearlyStatsTemplate.getId())
+        .makeCopy(
+          ("Weekend Days OHS Stat tracking information "
+            + yearStr),
+          yearFolder);
+      const site = SitesApp.getSiteByUrl(
+        PropertiesService.getScriptProperties().getProperty("googleSiteUrl")
+      );
+      const ohsStatsListPage = site.getChildByName("ohs-stats");
+      const values = [
+        "<a href=\"" + yearlyStatsFile.getUrl() + ">"
+        + yearStr + " OHS Stats</a>"
+      ];
+      ohsStatsListPage.addListItem(values);
+
+      return yearlyStatsFile;
+    }
 
     /**
      * Returns a reference to the folder object, for the current year, and its
@@ -64,13 +97,34 @@ const MonthlyRun = (
       const yearlyStatsFile = (
         (fileIterator.hasNext() === true)
         ? fileIterator.next()
-        : DriveApp.getFileById(yearlyStatsTemplate.getId())
-        .makeCopy(
-          ("Weekend Days OHS Stat tracking information "
-            + yearStr),
-          yearFolder));
+        : addYearlyStatsFile(yearlyStatsTemplate, yearStr, yearFolder));
 
       return [yearFolder, yearlyStatsFile];
+    }
+
+    /**
+     *
+     * @param {*} codeMoveTemplate
+     * @param {*} codeMoveSheetName
+     * @param {*} yearFolder
+     */
+    function addCodeMoveFile(codeMoveTemplate, codeMoveSheetName, yearFolder) {
+      const codeMoveFile = codeMoveTemplate.makeCopy(
+        codeMoveSheetName, yearFolder
+        );
+      const site = SitesApp.getSiteByUrl(
+        PropertiesService.getScriptProperties().getProperty("googleSiteUrl")
+      );
+      const codeMoveCountsPage = site.getChildByName("code-move-counts");
+      // add a yearly subpage
+      // add to subpate's list of monthly urls
+      const values = [
+        "<a href=\"" + yearlyStatsFile.getUrl() + ">"
+        + yearStr + " OHS Stats</a>"
+      ];
+      ohsStatsListPage.addListItem(values);
+
+      return codeMoveFile;
     }
 
     /**
@@ -97,7 +151,7 @@ const MonthlyRun = (
       const codeMoveFile = (
         (yearMonthFileFound)
         ? fileIterator.next()
-        : codeMoveTemplate.makeCopy(codeMoveSheetName, yearFolder));
+        : addCodeMoveFile(codeMoveTemplate, codeMoveSheetName, yearFolder));
       let spreadsheet = {};
 
       // edit new month spreadsheets
@@ -343,6 +397,6 @@ const MonthlyRun = (
       updateYearlyStatsFile
     });
 
-  }(DriveApp, PropertiesService, SpreadsheetApp));
+  }(DriveApp, PropertiesService, SitesApp, SpreadsheetApp));
 
 /******************************************************************************/
