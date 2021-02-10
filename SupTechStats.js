4fa9095fa4d781ccdc1515b60601c72aa7492eff
Kevin Griffin <kgriffin@meditech.com>
2021-02-10 13:01:06 -0500

    wip

--- SupTechStats.js
+++ SupTechStats.js
@@ -15,6 +15,49 @@ const SupTechStats = (
 
   function (DriveApp, PropertiesService, SitesApp, SpreadsheetApp) {
 
+    function pathwaysGetDataFormula(deliveriesSs, yearStr, nextYearStr) {
+      return `=QUERY(IMPORTRANGE("\
+      ${deliveriesSs.getUrl()}","Sheet1!A10:G"),\
+      "Select Col1, Col2, Col3, Col5, Col6 Where\
+      (Col5 >= date '${yearStr}-01-01' AND Col5 < date '${nextYearStr}-01-01')\
+      OR\
+      (Col6 >= date '${yearStr}-01-01' AND Col6 < date '${nextYearStr}-01-01')\
+      AND\
+      (dayOfWeek(Col5)=1 OR dayOfWeek(Col5)>=2 OR dayOfWeek(Col5)=6 OR\
+      dayOfWeek(Col5)>=7 OR dayOfWeek(Col6)=1 OR dayOfWeek(Col6)>=2 OR\
+      dayOfWeek(Col6)=6 OR dayOfWeek(Col6)>=7)")`;
+    }
+
+    function pathwaysDisplayDataFormula(tabArr, yearNum, monthNum, endOfMonth) {
+      return `=IFERROR(ISNA(QUERY('${tabArr[0]}'!A3:F,"Select * WHERE\
+        year(D)=${yearNum}\
+        AND (D >= date '${yearNum}-${monthNum}-1'\
+        AND D <= date '${yearNum}-${monthNum}-${endOfMonth}')\
+        AND (dayOfWeek(D)=1 OR dayOfWeek(D)=2 OR dayOfWeek(D)=6\
+        OR dayOfWeek(D)=7)")),0,ROWS(QUERY('${tabArr[0]}'!A3:F,"Select *\
+        WHERE year(D)=${yearNum}/
+        AND (D >= date '${yearNum}-${monthNum}-01'\
+        AND D <= date '${yearNum}-${monthNum}-${endOfMonth}')\
+        AND (dayOfWeek(D)=1 OR dayOfWeek(D)=2\
+        OR dayOfWeek(D)=6\
+        OR dayOfWeek(D)=7)")))+IF(ISERR(QUERY('${tabArr[0]}'\
+        !A3:F,"Select * WHERE year(E)=${yearNum}\
+        AND (E >= date '${yearNum}-${monthNum}-01'\
+        AND E <= date '${yearNum}-${monthNum}-${endOfMonth}')\
+        AND (dayOfWeek(E)=1 OR dayOfWeek(E)=2 OR dayOfWeek(E)=6\
+        OR dayOfWeek(E)=7)")),0,ROWS(QUERY('${tabArr[0]}'!A3:F,"Select\
+        * WHERE year(E)=${yearNum} AND (E >= date '${yearNum}-${monthNum}-01'\
+        AND E <= date '${yearNum}-${monthNum}-${endOfMonth}')\
+        AND (dayOfWeek(E)=1 OR dayOfWeek(E)=2\
+        OR dayOfWeek(E)=6 OR dayOfWeek(E)=7)"))),"")`;
+    }
+
+    genericDisplayDataFormula(sheetName, yearNum, monthNum, endOfMonth) {
+      return `=COUNTIFS('${sheetName}'!$A:$A,\
+      ">="&Date(${yearNum},${monthNum},1),'${sheetName}'!$A:$A,\
+      "<"&Date(${yearNum},${monthNum},${endOfMonth}))`;
+    }
+
     function yearlyInit(yearlySupTechFile, yearStr) {
       const spreadsheet = SpreadsheetApp.openById(yearlySupTechFile.getId());
       const indexSheet = spreadsheet.getSheetByName("Index");
@@ -33,16 +76,9 @@ const SupTechStats = (
 
       // special case for 6.x Pathway Code Deliveries
       spreadsheet.getSheetByName("6.x Pathway Code Deliveries")
-        .getRange("A3").setFormula(`=QUERY(IMPORTRANGE("\
-      ${deliveriesSs.getUrl()}","Sheet1!A10:G"),\
-      "Select Col1, Col2, Col3, Col5, Col6 Where\
-      (Col5 >= date '${yearStr}-01-01' AND Col5 < date '${nextYearStr}-01-01')\
-      OR\
-      (Col6 >= date '${yearStr}-01-01' AND Col6 < date '${nextYearStr}-01-01')\
-      AND\
-      (dayOfWeek(Col5)=1 OR dayOfWeek(Col5)>=2 OR dayOfWeek(Col5)=6 OR\
-      dayOfWeek(Col5)>=7 OR dayOfWeek(Col6)=1 OR dayOfWeek(Col6)>=2 OR\
-      dayOfWeek(Col6)=6 OR dayOfWeek(Col6)>=7)")`);
+        .getRange("A3").setFormula(
+          pathwaysGetDataFormula(deliveriesSs, yearStr, nextYearStr)
+        );
 
       // construct formulas for current year
       Array.from({
@@ -52,44 +88,45 @@ const SupTechStats = (
         const yearNum = Number(yearStr);
         const monthNum = index + 1;
         const endOfMonth = new Date(yearStr, monthNum, 0).getDate();
-        // TODO: add this to a range on ss
-        `=IFERROR(ISNA(QUERY('${tabArr[0]}'!A3:F,"Select * WHERE\
-        year(D)=${yearNum}\
-        AND (D >= date '${yearNum}-${monthNum}-1'\
-        AND D <= date '${yearNum}-${monthNum}-${endOfMonth}')\
-        AND (dayOfWeek(D)=1 OR dayOfWeek(D)=2 OR dayOfWeek(D)=6\
-        OR dayOfWeek(D)=7)")),0,ROWS(QUERY('${tabArr[0]}'!A3:F,"Select *\
-        WHERE year(D)=${yearNum} AND (D >= date '${yearNum}-${monthNum}-01'\
-        AND D <= date '${yearNum}-${monthNum}-${endOfMonth}')\
-        AND (dayOfWeek(D)=1 OR dayOfWeek(D)=2\
-        OR dayOfWeek(D)=6 OR dayOfWeek(D)=7)")))+IF(ISERR(QUERY('${tabArr[0]}'\
-        !A3:F,"Select * WHERE year(E)=${yearNum}\
-        AND (E >= date '${yearNum}-${monthNum}-01'\
-        AND E <= date '${yearNum}-${monthNum}-${endOfMonth}')\
-        AND (dayOfWeek(E)=1 OR dayOfWeek(E)=2 OR dayOfWeek(E)=6\
-        OR dayOfWeek(E)=7)")),0,ROWS(QUERY('${tabArr[0]}'!A3:F,"Select * WHERE\
-        year(E)=${yearNum} AND (E >= date '${yearNum}-${monthNum}-01'\
-        AND E <= date '${yearNum}-${monthNum}-${endOfMonth}')\
-        AND (dayOfWeek(E)=1 OR dayOfWeek(E)=2\
-        OR dayOfWeek(E)=6 OR dayOfWeek(E)=7)"))),"")`;
-        //
-        `=COUNT(UNIQUE(FILTER('CSCT Messages'!$B:$B,'CSCT Messages'!$E:$E >= Date(2021,1,1),'CSCT Messages'!$E:$E <= Date(2021,2,1))))`;
-        `=COUNTIFS('Data Recoveries'!$A:$A, ">="&Date(2021,1,1),'Data Recoveries'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Development Projects (CSTS)'!$A:$A, ">="&Date(2021,1,1),'Development Projects (CSTS)'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Health Check'!$A:$A, ">="&Date(2021,1,1),'Health Check'!$A:$A,"<"&Date(2021,2,1))`;
-        `=SUMIFS('Health Check'!E:E,'Health Check'!A:A,">="&Date(2021,1,1),'Health Check'!A:A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Infrastructure Projects'!$A:$A, ">="&Date(2021,1,1),'Infrastructure Projects'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Large Scale Projects'!$A:$A, ">="&Date(2021,1,1),'Large Scale Projects'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('LIVE Tasks Support'!$A:$A, ">="&Date(2021,1,1),'LIVE Tasks Support'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS(MaaS!$A:$A, ">="&Date(2021,1,1),MaaS!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Maintenance/Downtime Projects'!$A:$A, ">="&Date(2021,1,1),'Maintenance/Downtime Projects'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Scheduled Projects'!$A:$A, ">="&Date(2021,1,1),'Scheduled Projects'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Stipend/Non Stipend'!$A:$A, ">="&Date(2021,1,1),'Stipend/Non Stipend'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Tech Code Moves'!$A:$A, ">="&Date(2021,1,1),'Tech Code Moves'!$A:$A,"<"&Date(2021,2,1))`;
-        `=COUNTIFS('Updates Supported'!$D:$D, ">="&Date(2021,1,1),'Updates Supported'!$D:$D,"<"&Date(2021,2,1),'Updates Supported'!$G:$G,"Magic",'Updates Supported'!$K:$K,"Yes")`;
-        `=COUNTIFS('Updates Supported'!$D:$D, ">="&Date(2021,1,1),'Updates Supported'!$D:$D,"<"&Date(2021,2,1),'Updates Supported'!$G:$G,"CS",'Updates Supported'!$K:$K,"Yes")`;
-        `=COUNTIFS('Updates Supported'!$D:$D, ">="&Date(2021,1,1),'Updates Supported'!$D:$D,"<"&Date(2021,2,1),'Updates Supported'!$G:$G,"6.08",'Updates Supported'!$K:$K,"Yes")+COUNTIFS('Updates Supported'!$D:$D, ">="&Date(2021,1,1),'Updates Supported'!$D:$D,"<"&Date(2021,2,1),'Updates Supported'!$G:$G,"6.15",'Updates Supported'!$K:$K,"Yes")+COUNTIFS('Updates Supported'!$D:$D, ">="&Date(2021,1,1),'Updates Supported'!$D:$D,"<"&Date(2021,2,1),'Updates Supported'!$G:$G,"Expanse",'Updates Supported'!$K:$K,"Yes")`;
-        `=COUNTIFS('UWI Code Moves'!$A:$A, ">="&Date(2021,1,1),'UWI Code Moves'!$A:$A,"<"&Date(2021,2,1))`;
+        const colLetter = String.fromCharCode(66 + index);
+        // 6.x Pathway Code Deliveries
+        indexSheet.getRange(`"${colLetter}:2"`).setFormula(
+          pathwaysDisplayDataFormula(tabArr, yearNum, monthNum, endOfMonth)
+        );
+        // CSCT Messages
+        `=COUNT(UNIQUE(FILTER('${tabArr[1]}'!$B:$B,'${tabArr[1]}'!$E:$E >= Date(2021,1,1),'${tabArr[1]}'!$E:$E <= Date(2021,2,1))))`;
+        // Data Recoveries
+        genericDisplayDataFormula(tabArr[2], yearNum, monthNum, endOfMonth)
+        // Development Projects (CSTS)
+        genericDisplayDataFormula(tabArr[3], yearNum, monthNum, endOfMonth)
+        // Health Check
+        genericDisplayDataFormula(tabArr[4], yearNum, monthNum, endOfMonth)
+        // Health Check - Resolution
+        `=SUMIFS('${tabArr[5]}'!E:E,'${tabArr[5]}'!A:A,">="&Date(2021,1,1),'${tabArr[5]}'!A:A,"<"&Date(2021,2,1))`;
+        // Infrastructure Projects
+        genericDisplayDataFormula(tabArr[6], yearNum, monthNum, endOfMonth)
+        // Large Scale Projects
+        genericDisplayDataFormula(tabArr[7], yearNum, monthNum, endOfMonth)
+        // LIVE Tasks Support
+        genericDisplayDataFormula(tabArr[8], yearNum, monthNum, endOfMonth)
+        // MaaS
+        genericDisplayDataFormula(tabArr[9], yearNum, monthNum, endOfMonth)
+        // Maintenance/Downtime Projects
+        genericDisplayDataFormula(tabArr[10], yearNum, monthNum, endOfMonth)
+        // Scheduled Projects
+        genericDisplayDataFormula(tabArr[11], yearNum, monthNum, endOfMonth)
+        // Stipend/Non Stipend
+        genericDisplayDataFormula(tabArr[12], yearNum, monthNum, endOfMonth)
+        // Tech Code Moves
+        genericDisplayDataFormula(tabArr[13], yearNum, monthNum, endOfMonth)
+        // Updates Supported (MG)
+        `=COUNTIFS('${tabArr[14]}'!$D:$D, ">="&Date(2021,1,1),'${tabArr[14]}'!$D:$D,"<"&Date(2021,2,1),'${tabArr[14]}'!$G:$G,"Magic",'${tabArr[14]}'!$K:$K,"Yes")`;
+        // Updates Supported (CS)
+        `=COUNTIFS('${tabArr[15]}'!$D:$D, ">="&Date(2021,1,1),'${tabArr[15]}'!$D:$D,"<"&Date(2021,2,1),'${tabArr[15]}'!$G:$G,"CS",'${tabArr[15]}'!$K:$K,"Yes")`;
+        // Updates Supported (Exp)
+        `=COUNTIFS('${tabArr[16]}'!$D:$D, ">="&Date(2021,1,1),'${tabArr[16]}'!$D:$D,"<"&Date(2021,2,1),'${tabArr[16]}'!$G:$G,"6.08",'${tabArr[16]}'!$K:$K,"Yes")+COUNTIFS('${tabArr[16]}'!$D:$D, ">="&Date(2021,1,1),'${tabArr[16]}'!$D:$D,"<"&Date(2021,2,1),'${tabArr[16]}'!$G:$G,"6.15",'${tabArr[16]}'!$K:$K,"Yes")+COUNTIFS('${tabArr[16]}'!$D:$D, ">="&Date(2021,1,1),'${tabArr[16]}'!$D:$D,"<"&Date(2021,2,1),'${tabArr[16]}'!$G:$G,"Expanse",'${tabArr[16]}'!$K:$K,"Yes")`;
+        // UWI Code Moves
+        genericDisplayDataFormula(tabArr[17], yearNum, monthNum, endOfMonth)
       });
 
       return undefined;
